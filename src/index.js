6a6924b114537511f82a76679c5b4f1a6a287a38
Laura Curley <info@lauracurley.com>
2016-08-04 16:28:09 -0700

    Set up basic stylesheet

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
-import '../node_modules/bootstrap/dist/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
   <Router history={browserHistory} routes={routes}/>,
-  document.getElementsById('app')
+  document.getElementById('app')
 );
