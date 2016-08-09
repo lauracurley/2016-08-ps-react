4db424f2af1b03754744cbaacbad3bbc8b2570d9
Laura Curley <info@lauracurley.com>
2016-08-08 17:29:29 -0700

    Update index to use new actions

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,10 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function courseReducer(state = initialState.courses, action) {
+export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state, Object.assign({}, action.course)];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
